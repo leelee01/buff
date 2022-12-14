2d8298d60f4ba661fe363abfb04a29871c81d539
1202l <1202l@DESKTOP-I27P2OF>
2022-12-15 00:50:30 +0900

    프로젝트 생성

---
+++ src/test/java/com/main/buff/BuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.buff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
